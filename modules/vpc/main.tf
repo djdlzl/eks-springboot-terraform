ddd884d4f1a47ff8a9cea948c3a94cab346885d4
Jaewoo Cho <djdlzl93@gmail.com>
2025-05-15 00:02:20 +0900

    check a syntax

---
+++ modules/vpc/main.tf
@@ -0,0 +1,30 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.availability_zones
+  public_subnets  = var.public_subnet_cidrs
+  private_subnets = var.private_subnet_cidrs
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
+
+  enable_vpn_gateway = false
+
+  # Kubernetes-specific tags for EKS
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  tags = {
+    Environment = var.environment
+    Terraform   = "true"
+  }
+}
